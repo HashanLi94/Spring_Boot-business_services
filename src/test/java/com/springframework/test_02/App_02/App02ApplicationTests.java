95de599c4034567b0d3d74e27b09fe677029f2d5
HashanLi94 <hashanduplessis@gmail.com>
2019-12-16 02:33:09 +0530

    Create App02ApplicationTests.java

---
+++ src/test/java/com/springframework/test_02/App_02/App02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.test_02.App_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
