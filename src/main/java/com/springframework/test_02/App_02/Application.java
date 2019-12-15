96033fe9709a2b5052c9c4348b78deb444d5994e
HashanLi94 <hashanduplessis@gmail.com>
2019-12-16 02:31:40 +0530

    Create Application.java

---
+++ src/main/java/com/springframework/test_02/App_02/Application.java
@@ -0,0 +1,15 @@
+package com.springframework.test_02.App_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
